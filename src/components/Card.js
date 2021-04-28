7ef9c9da612af2b2cdce7d3632840b2cde5c964c
mikkelwilk <mikkelwilk@gmail.com>
2021-04-27 22:31:29 -0600

    update

--- src/components/Card.js
+++ src/components/Card.js
@@ -16,28 +16,39 @@ function Card(props) {
           {...provided.draggableProps}
           ref={provided.innerRef}
         >
-          <div
-            onClick={() =>
-              window.open(
-                props.platforms.find(({ pid }) => pid === props.card.pid).base +
-                  props.card.value,
-                "_blank"
-              )
-            }
-          >
-            <h3>
-              {props.platforms.find(({ pid }) => pid === props.card.pid).name}
-            </h3>
-            <p>
-              {props.platforms.find(({ pid }) => pid === props.card.pid)
-                .useBase &&
-                props.platforms.find(({ pid }) => pid === props.card.pid).base}
-              {props.card.value}
-            </p>
+          <div className="CardContentContainer">
+            <img
+              className="CardIcon"
+              src={
+                props.platforms.find(({ pid }) => pid === props.card.pid).icon
+              }
+            />
+            <div
+              className="CardTextContainer"
+              onClick={() =>
+                window.open(
+                  props.platforms.find(({ pid }) => pid === props.card.pid)
+                    .base + props.card.value,
+                  "_blank"
+                )
+              }
+            >
+              <h3>
+                {props.platforms.find(({ pid }) => pid === props.card.pid).name}
+              </h3>
+              <p className="CardUrl">
+                {props.platforms.find(({ pid }) => pid === props.card.pid)
+                  .useBase &&
+                  props.platforms
+                    .find(({ pid }) => pid === props.card.pid)
+                    .base.replace(/(^\w+:|^)\/\//, "")}
+                {props.card.value.replace(/(^\w+:|^)\/\//, "")}
+              </p>
+            </div>
           </div>
           {isOwner(props.profile, props.user) && (
             <div {...provided.dragHandleProps}>
-              <MdDragHandle size="1.5em" />
+              <MdDragHandle size="2em" />
             </div>
           )}
         </div>
